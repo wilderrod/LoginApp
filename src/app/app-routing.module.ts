24250f5356c7b3450dcfc38b48266daeb03fef20
Wilder Rodriguez <wilder.rodriguezq@gmail.com>
2019-02-05 13:02:14 -0500

    rutas y navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import {HomeComponent} from './componentes/home/home.component';
 import {LoginComponent} from './componentes/login/login.component';
 import {RegisterComponent} from './componentes/register/register.component';
