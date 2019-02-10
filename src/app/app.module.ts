74636a01e3c01faaa62fd0c978ca01945df7d9c6
Wilder Rodriguez <wilder.rodriguezq@gmail.com>
2019-02-10 17:55:07 -0500

    facebook y gmail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
-//import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 //import { AngularFireModule } from '@angular/fire';
 import { AngularFireModule } from 'angularfire2';
